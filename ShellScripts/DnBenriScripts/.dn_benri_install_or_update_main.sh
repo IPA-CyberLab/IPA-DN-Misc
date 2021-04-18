bd46fd3c826290d1f833a6b36606a49722449be6
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-04-18 14:10:08 +0900

    update

--- ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
+++ ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
@@ -7,10 +7,14 @@ export DEBIAN_FRONTEND=noninteractive
 cat <<\EOF > /etc/profile.d/dn_benri_aliases.sh
 # by dnobori
 
+# テスト
+alias dn_test1='echo Test1 OK!'
+
+# 便利関数の更新
+alias dn_update_benri='bash -c "bash <( curl --raw https://raw.githubusercontent.com/IPA-CyberLab/IPA-DN-Misc/main/ShellScripts/DnBenriScripts/install.sh )" ; source /etc/profile.d/dn_benri_aliases.sh'
+
 # Docker 関係
 alias dn_docker_show_containers_and_volumes='docker system df -v | tee /dev/tty | xargs -n 1 -P 1 -IXXX bash --noprofile --norc -c '"'"'id=$(echo -n "XXX" | awk "{printf \"%s\", \$1;}"); if [ $(echo -n $id | wc -m) -eq 64 ]; then echo ; echo "### Volume Container List for:"; echo "Volume: XXX"; docker ps -a --filter volume="$id"; fi'"'"'; echo'
-
-alias dn_update_benri='bash -c "bash <( curl --raw https://raw.githubusercontent.com/IPA-CyberLab/IPA-DN-Misc/main/ShellScripts/DnBenriScripts/install.sh )" ; source /etc/profile.d/dn_benri_aliases.sh'
 
 EOF
 
@@ -18,6 +22,6 @@ chmod 755 /etc/profile.d/dn_benri_aliases.sh
 
 echo 
 
-echo Install OK !!
+echo Install or Update Benri Scripts OK !!
 
 
