406a9a739a72e9e100d1722367965045465f3a33
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-04-18 14:28:01 +0900

    a

--- ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
+++ ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
@@ -17,13 +17,12 @@ alias dn_update_benri='bash -c "bash <( curl --raw https://raw.githubusercontent
 alias dn_docker_show_containers_and_volumes='docker system df -v | tee /dev/tty | xargs -n 1 -P 1 -IXXX bash --noprofile --norc -c '"'"'id=$(echo -n "XXX" | awk "{printf \"%s\", \$1;}"); if [ $(echo -n $id | wc -m) -eq 64 ]; then echo ; echo "### Volume Container List for:"; echo "Volume: XXX"; docker ps -a --filter volume="$id"; fi'"'"'; echo'
 
 alias dn_docker_show_containers='docker ps -a'
-
 alias dn_docker_show_containers_size='docker ps -a -s'
-
 alias dn_docker_show_containers_running='docker ps'
-
 alias dn_docker_show_containers_running_size='docker ps -s'
 alias dn_docker_show_containers_size_running='docker ps -s'
+alias dn_docker_show_running_containers='docker ps'
+alias dn_docker_show_running_containers_size='docker ps -s'
 
 EOF
 
