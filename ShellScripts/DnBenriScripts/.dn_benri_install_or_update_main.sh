3b3120720841a8879fc248abf88dfa6aa6c2c398
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-04-18 14:27:01 +0900

    aa

--- ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
+++ ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
@@ -16,13 +16,14 @@ alias dn_update_benri='bash -c "bash <( curl --raw https://raw.githubusercontent
 # Docker 関係
 alias dn_docker_show_containers_and_volumes='docker system df -v | tee /dev/tty | xargs -n 1 -P 1 -IXXX bash --noprofile --norc -c '"'"'id=$(echo -n "XXX" | awk "{printf \"%s\", \$1;}"); if [ $(echo -n $id | wc -m) -eq 64 ]; then echo ; echo "### Volume Container List for:"; echo "Volume: XXX"; docker ps -a --filter volume="$id"; fi'"'"'; echo'
 
-alias dn_docker_show_containers='docker ps -a -s'
+alias dn_docker_show_containers='docker ps -a'
 
-alias dn_docker_show_containers_fast='docker ps -a'
+alias dn_docker_show_containers_size='docker ps -a -s'
 
-alias dn_docker_show_containers_running='docker ps -s'
+alias dn_docker_show_containers_running='docker ps'
 
-alias dn_docker_show_containers_running_fast='docker ps'
+alias dn_docker_show_containers_running_size='docker ps -s'
+alias dn_docker_show_containers_size_running='docker ps -s'
 
 EOF
 
