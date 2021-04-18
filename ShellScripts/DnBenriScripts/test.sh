d9182ee4b06b1eb02896880e24cdc8e9f038221c
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-04-18 13:07:07 +0900

    Init ねこさん

---
+++ ShellScripts/DnBenriScripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# How to run this script:
+# 
+# sudo bash -c "bash -x <(curl --raw https://raw.githubusercontent.com/IPA-CyberLab/IPA-DN-Misc/main/ShellScripts/DnBenriScripts/test.sh)"
+
+
+echo Hello World
+
