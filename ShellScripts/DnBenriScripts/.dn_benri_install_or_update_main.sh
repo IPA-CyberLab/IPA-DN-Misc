02c17068ec32753efba125476973ff827fbc8356
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-11-12 17:28:22 +0900

    a

--- ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
+++ ShellScripts/DnBenriScripts/.dn_benri_install_or_update_main.sh
@@ -117,6 +117,58 @@ function dn_docker_exec_bash()
 
 
 
+alias dn_copy_if_diff='python3 -c "
+import os
+import json
+import subprocess
+import inspect
+import sys
+import time as systime
+import argparse
+import shutil
+
+def IsSameFile(a: str, b: str):
+    if not os.path.exists(a):
+        return False
+
+    if not os.path.exists(b):
+        return False
+
+    with open(a, mode='"'"'rb'"'"') as f:
+        src_data = f.read()
+    
+    with open(b, mode='"'"'rb'"'"') as f:
+        dst_data = f.read()
+    
+    if len(src_data) != len(dst_data):
+        return False
+    
+    return src_data == dst_data
+
+if __name__ == '"'"'__main__'"'"':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(\"src\", metavar=\"<src>\", type=str, help=\"Source filename\")
+    parser.add_argument(\"-d\", dest=\"dst\", required=True,
+                        type=str, help=\"Destination filename\")
+
+    args = parser.parse_args()
+    src = args.src
+    dst = args.dst
+
+    try:
+        isSame = IsSameFile(src, dst)
+    except:
+        isSame = False
+
+    if not isSame:
+        shutil.copyfile(src, dst)
+
+"'
+
+
+
+
+
 
 
 alias dn_replace_str='python3 -c "
